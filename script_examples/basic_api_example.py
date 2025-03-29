1e5cea977b4f5cd5c11c9acb1f47a423d42f3ffb
vietnguy <viet.nguy.95@gmail.com>
2025-03-29 01:04:54 -0400

    stable diffusion fork

---
+++ script_examples/basic_api_example.py
@@ -0,0 +1,34 @@
+import json
+from urllib import request
+import random
+
+#This is the ComfyUI api prompt format.
+
+#If you want it for a specific workflow you can "enable dev mode options"
+#in the settings of the UI (gear beside the "Queue Size: ") this will enable
+#a button on the UI to save workflows in api format.
+
+#keep in mind ComfyUI is pre alpha software so this format will change a bit.
+
+#this is the one for the default workflow
+
+
+def queue_prompt(prompt):
+    print("called")
+    p = {"prompt": prompt}
+    data = json.dumps(p).encode('utf-8')
+    req =  request.Request("http://127.0.0.1:8188/prompt", data=data)
+    request.urlopen(req)
+
+file_path = './api_prompts/elf_dryad.json'
+batch_size = 4
+
+
+with open(file_path, 'r') as file:
+    prompt_text = file.read()
+    prompt = json.loads(prompt_text)
+
+    for iteration in range(batch_size):
+        prompt["3"]["inputs"]["seed"] = random.randint(1,4294967294)
+        queue_prompt(prompt)
+
