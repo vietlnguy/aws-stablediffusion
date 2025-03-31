8e21362c716dfd4f6738c8d9ab15f54870101620
vietnguy <viet.nguy.95@gmail.com>
2025-03-31 02:31:08 -0400

    try sync models script

--- sync-models.py
+++ sync-models.py
@@ -1,11 +1,11 @@
 import json
 import os
 
-with open('model-requirements.json') as file:
+with open('/aws-stablediffusion/model-requirements.json') as file:
     temp = json.load(file)
     local = temp['model_ids']
 
-with open('model-requirements-remote.json') as file:
+with open('/mnt/data/model-requirements-remote.json') as file:
     temp = json.load(file)
     remote = temp['model_ids']
 
@@ -13,6 +13,4 @@ diff = list(set(local) - set(remote))
 
 for id in diff:
     command = "wget \"civitai.com/api/download/models/" + str(id) + "?token=" + os.environ['CIVITAI_KEY'] + "\" --content-disposition -P /mnt/data/models/checkpoints/"
-    print(os.environ)
-    print(command)
-    #os.system(command)
+    os.system(command)
